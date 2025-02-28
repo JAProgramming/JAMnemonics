c7cd1f3d8da8a5b405e08c7215a68978f398a267
Justin Allman <126277294+JAProgramming@users.noreply.github.com>
2025-02-28 14:15:19 -0500

    Update WordList.swift

--- Sources/PhoneNumberMnemonics/WordList.swift
+++ Sources/PhoneNumberMnemonics/WordList.swift
@@ -4,5 +4,20 @@
 import Foundation
 
 // YOU FILL IN HERE
+struct Fruit {
+    let word: [String]
+    
+    init() {
+        do {
+            let fileName: String = "words.txt"
+            let readFile = try String(contentsOfFile: fileName)
+            word = readFile.components(separatedBy: "\n")
+        } catch let error {
+            Swift.print("Fatal Error: \(error.localizedDescription)")
+            word = []
+        }
+    }
+}
+
 // You'll want to create a mechanism here for loading and querying
 // words.txt. It's up to you how you do this. You may consider a struct.
