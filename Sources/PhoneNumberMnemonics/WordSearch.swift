928b9681e9d37b3e268f8040733780060a1d2862
Justin Allman <126277294+JAProgramming@users.noreply.github.com>
2025-03-04 14:05:44 -0500

    Update WordSearch.swift

--- Sources/PhoneNumberMnemonics/WordSearch.swift
+++ Sources/PhoneNumberMnemonics/WordSearch.swift
@@ -50,7 +50,7 @@ public func wordsInString(_ string: String, ofMinLength length: UInt) -> [String
     // YOU FILL IN HERE
     let wordList = Fruit()
     let newPossibles = possibles(for: string)
-    let smallerList = wordList.filter($0.count > length)
+    let smallerList = wordList.filter {$0.count > 5)
     
     return smallerList
 }
